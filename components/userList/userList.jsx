c9f8bdcfa2f9d569cc27ece459e1647daea3fad7
Shiv Patel <spate373@charlotte.edu>
2026-03-25 09:32:09 -0400

    Fetch and display users in UserList component
    Implemented UserList.jsx to fetch and display user list with routing

--- components/userList/userList.jsx
+++ components/userList/userList.jsx
@@ -15,33 +15,34 @@ import './userList.css';
 class UserList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      users: [],
+    };
+  }
+
+  componentDidMount() {
+    // Get users from model (Phase 1)
+    const userList = window.models.userListModel();
+    this.setState({ users: userList });
   }
 
   render() {
     return (
-      <div>
-        <Typography variant="body1">
-          This is the user list, which takes up 3/12 of the window.
-          You might choose to use <a href="https://mui.com/components/lists/">Lists</a> and <a href="https://mui.com/components/dividers/">Dividers</a> to
-          display your users like so:
-        </Typography>
+      <div className="user-list">
+        <Typography variant="h6">Users</Typography>
+
         <List component="nav">
-          <ListItem>
-            <ListItemText primary="Item #1" />
-          </ListItem>
-          <Divider />
-          <ListItem>
-            <ListItemText primary="Item #2" />
-          </ListItem>
-          <Divider />
-          <ListItem>
-            <ListItemText primary="Item #3" />
-          </ListItem>
-          <Divider />
+          {this.state.users.map((user) => (
+            <div key={user._id}>
+              <ListItem button component={Link} to={`/users/${user._id}`}>
+                <ListItemText
+                  primary={`${user.first_name} ${user.last_name}`}
+                />
+              </ListItem>
+              <Divider />
+            </div>
+          ))}
         </List>
-        <Typography variant="body1">
-          The model comes in from window.models.userListModel()
-        </Typography>
       </div>
     );
   }
