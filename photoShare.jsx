bfe06f88ebdea08745b4e3af32cb16792004ade6
Sunny2737 <Sullah1@charlotte.edu>
2026-04-21 18:20:02 -0400

    Update photoShare.jsx

--- photoShare.jsx
+++ photoShare.jsx
@@ -1,54 +1,183 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
 import {
-  HashRouter, Route, Switch
-} from 'react-router-dom';
+  HashRouter,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import {
-  Grid, Paper
-} from '@mui/material';
-import './styles/main.css';
+  Grid,
+  Paper,
+} from "@mui/material";
+import "./styles/main.css";
 
 // Components
-import TopBar from './components/topBar/TopBar';
-import UserDetail from './components/userDetail/userDetail';
-import UserList from './components/userList/userList';
-import UserPhotos from './components/userPhotos/userPhotos';
+import TopBar from "./components/topBar/TopBar";
+import UserDetail from "./components/userDetail/userDetail";
+import UserList from "./components/userList/userList";
+import UserPhotos from "./components/userPhotos/userPhotos";
+import LoginRegister from "./components/loginRegister/loginRegister";
 
 class PhotoShare extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: null,
+      mainContent: "",
+      photoUploadMessage: "",
+    };
+  }
+
+  handleLogin = (user) => {
+    this.setState({
+      currentUser: user,
+      mainContent: "",
+      photoUploadMessage: "",
+    });
+  };
+
+  handleLogout = async () => {
+    try {
+      await axios.post("/admin/logout", {});
+    } catch (err) {
+      // Ignore backend logout error and still clear frontend state
+      // so the user can get back to login screen cleanly.
+    }
+
+    this.setState({
+      currentUser: null,
+      mainContent: "",
+      photoUploadMessage: "",
+    });
+
+    window.location.hash = "#/login-register";
+  };
+
+  changeMainContent = (text) => {
+    this.setState({
+      mainContent: text || "",
+    });
+  };
+
+  handlePhotoUpload = async (file) => {
+    if (!file) {
+      throw new Error("Please choose a photo first.");
+    }
+
+    const domForm = new FormData();
+    domForm.append("uploadedphoto", file);
+
+    await axios.post("/photos/new", domForm);
+
+    this.setState({
+      photoUploadMessage: "Photo uploaded successfully.",
+    });
+
+    if (this.state.currentUser && this.state.currentUser._id) {
+      window.location.hash = `#/photos/${this.state.currentUser._id}`;
+    }
+  };
+
+  renderProtectedRoute = (renderFn) => {
+    const { currentUser } = this.state;
+
+    if (!currentUser) {
+      return <Redirect to="/login-register" />;
+    }
+
+    return renderFn();
+  };
+
   render() {
+    const { currentUser, mainContent } = this.state;
+
     return (
       <HashRouter>
         <Grid container spacing={2}>
-          
-          {/* Top Bar */}
           <Grid item xs={12}>
-            <TopBar />
+            <TopBar
+              currentUser={currentUser}
+              main_content={mainContent}
+              onLogout={this.handleLogout}
+              onPhotoUpload={this.handlePhotoUpload}
+            />
           </Grid>
 
-          <div className="main-topbar-buffer"/>
+          <div className="main-topbar-buffer" />
 
-          {/* Sidebar */}
           <Grid item sm={3}>
             <Paper className="main-grid-item">
-              <UserList />
+              {currentUser ? <UserList /> : null}
             </Paper>
           </Grid>
 
-          {/* Main Content */}
           <Grid item sm={9}>
             <Paper className="main-grid-item">
               <Switch>
-                <Route path="/users/:userId"
-                  render={props => <UserDetail {...props} />}
+                <Route
+                  exact
+                  path="/login-register"
+                  render={(props) => (
+                    currentUser ? (
+                      <Redirect to={`/users/${currentUser._id}`} />
+                    ) : (
+                      <LoginRegister
+                        {...props}
+                        handleLogin={this.handleLogin}
+                      />
+                    )
+                  )}
                 />
-                <Route path="/photos/:userId"
-                  render={props => <UserPhotos {...props} />}
+
+                <Route
+                  exact
+                  path="/users/:userId"
+                  render={(props) => this.renderProtectedRoute(() => (
+                    <UserDetail
+                      {...props}
+                      changeMainContent={this.changeMainContent}
+                    />
+                  ))}
                 />
-                <Route path="/users" component={UserList} />
+
+                <Route
+                  exact
+                  path="/photos/:userId"
+                  render={(props) => this.renderProtectedRoute(() => (
+                    <UserPhotos
+                      {...props}
+                      setTopBarContext={this.changeMainContent}
+                    />
+                  ))}
+                />
+
+                <Route
+                  exact
+                  path="/users"
+                  render={() => (
+                    currentUser ? (
+                      <Redirect to={`/users/${currentUser._id}`} />
+                    ) : (
+                      <Redirect to="/login-register" />
+                    )
+                  )}
+                />
+
+                <Route
+                  path="/"
+                  render={() => (
+                    currentUser ? (
+                      <Redirect to={`/users/${currentUser._id}`} />
+                    ) : (
+                      <Redirect to="/login-register" />
+                    )
+                  )}
+                />
               </Switch>
             </Paper>
           </Grid>
-
         </Grid>
       </HashRouter>
     );
@@ -57,5 +186,5 @@ class PhotoShare extends React.Component {
 
 ReactDOM.render(
   <PhotoShare />,
-  document.getElementById('photoshareapp'),
+  document.getElementById("photoshareapp")
 );
